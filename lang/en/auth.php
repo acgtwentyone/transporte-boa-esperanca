ab525f485a63a133a24fdac17981340c608d0169
António Gonçalves <acgtwentyone@gmail.com>
2022-05-19 12:18:43 -0100

    Update auth.php

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -12,9 +12,10 @@ return [
     | these language lines according to your application's requirements.
     |
     */
-
+    
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'email' => 'Email',
+    'something_wrong' => 'Oppss. Something went wrong.'
 ];
