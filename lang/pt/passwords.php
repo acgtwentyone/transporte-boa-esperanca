d386c1c546cf33a179675f999ade2a24fc457e7b
António Gonçalves <acgtwentyone@gmail.com>
2022-05-19 09:49:04 -0100

    Create passwords.php

---
+++ lang/pt/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'A sua palavra passe foi redefinida!',
+    'sent' => 'Enviamos um email para redefinir a sua palavra passe.',
+    'throttled' => 'Por favor espere antes de tentar novamente.',
+    'token' => 'Este token de redefinição de password é inválido.',
+    'user' => "Não conseguimos localizar um utilizador com este endereço de email.",
+
+];
