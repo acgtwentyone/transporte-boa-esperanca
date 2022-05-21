833c054d5e192a1e7809e58c226c3f4ea4d59205
António Gonçalves <acgtwentyone@gmail.com>
2022-05-21 20:05:32 -0100

    Create custom.php

---
+++ config/custom.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    "SUPER_ADMIN_EMAIL" => env("SUPER_ADMIN_EMAIL"),
+    "SUPER_ADMIN_PASSWORD" => env("SUPER_ADMIN_PASSWORD"),
+    "SUPER_ADMIN_USERNAME" => env("SUPER_ADMIN_USERNAME")
+];
