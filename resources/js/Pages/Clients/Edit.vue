122223ccdd533bcff62aadefbfffc6c61c916822
António Gonçalves <acgtwentyone@gmail.com>
2022-05-28 18:07:36 -0100

    Create Edit.vue

---
+++ resources/js/Pages/Clients/Edit.vue
@@ -0,0 +1,21 @@
+<script setup>
+import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
+import { Head, usePage } from "@inertiajs/inertia-vue3";
+import { computed } from "vue";
+</script>
+
+<template>
+  <Head title="Dashboard" />
+
+  <BreezeAuthenticatedLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Alterar Cliente
+      </h2>
+    </template>
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <h1 class="p-4 text-base">Preencha os campos</h1>
+    </div>
+  </BreezeAuthenticatedLayout>
+</template>
