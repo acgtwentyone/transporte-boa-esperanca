2798e4a134c3d4c3f5f847d7f05356d1dd86f8c7
António Gonçalves <acgtwentyone@gmail.com>
2022-05-28 18:04:03 -0100

    Update Login.vue

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -24,7 +24,9 @@ const form = useForm({
 
 const submit = () => {
   form.post(route("login"), {
-    // onFinish: () => form.reset('password'),
+    onSuccess: () => {
+      form.reset()
+    },
   });
 };
 </script>
