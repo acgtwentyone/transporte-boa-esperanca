ec30a9e757d934df40c2ef145f044af2bb739565
António Gonçalves <acgtwentyone@gmail.com>
2022-05-31 22:32:08 -0100

    Update WorkRequest.php

--- app/Http/Requests/WorkRequest.php
+++ app/Http/Requests/WorkRequest.php
@@ -17,15 +17,17 @@ class WorkRequest extends FormRequest
             'date' => 'required',
             'material' => 'required',
             'place' => 'required',
+            'client_id' => 'required',
         ];
     }
 
     public function messages()
     {
         return [
-            'date.required' => 'Data é obrigatório',
-            'material.required' => 'Material é obrigatório',
-            'place.required' => 'Lugar é obrigatório',
+            'date.required' => 'Data é obrigatório.',
+            'material.required' => 'Material é obrigatório.',
+            'place.required' => 'Lugar é obrigatório.',
+            'client_id.required' => 'Não foi possível identificar o cliente.',
         ];
     }
 }
