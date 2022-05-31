16c0d39fc45bc03702426ff46c8609d51a203606
António Gonçalves <acgtwentyone@gmail.com>
2022-05-31 00:30:55 -0100

    Create Create.vue

---
+++ resources/js/Pages/Works/Create.vue
@@ -0,0 +1,19 @@
+<script setup>
+import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
+import { Head, useForm, usePage } from "@inertiajs/inertia-vue3";
+import { computed } from "vue";
+
+import Input from "@/Components/Input.vue";
+import InputError from "@/Components/InputError.vue";
+import Label from "@/Components/Label.vue";
+import Button from "@/Components/Button.vue";
+
+import CreateEdit from '@/Pages/Clients/Partials/CreateEdit.vue';
+
+const errors = computed(() => usePage().props.value.errors);
+const hasErrors = computed(() => Object.keys(errors.value).length > 0);
+</script>
+
+<template>
+  <CreateEdit />
+</template>
