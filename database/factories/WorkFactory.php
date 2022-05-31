c934e7920aabd06b5a9a0996424d7e0f29f00ad1
António Gonçalves <acgtwentyone@gmail.com>
2022-05-31 00:15:25 -0100

    Create WorkFactory.php

---
+++ database/factories/WorkFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Work>
+ */
+class WorkFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'date' => "02-09-2022",
+            'material' => Str::random(10),
+            'place' => $this->faker->name(),
+            'freight_value' => 2000,
+            'paid' => 0,
+            'debt_value' => 0,
+            'price' => 30000,
+            'debt_date' => "02-09-2022",
+        ];
+    }
+}
