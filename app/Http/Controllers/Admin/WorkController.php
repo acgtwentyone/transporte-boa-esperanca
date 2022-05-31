9ef0bc180cf0ee15eb0bbc54fc6a4e8f0e517673
António Gonçalves <acgtwentyone@gmail.com>
2022-05-31 00:15:13 -0100

    Create WorkController.php

---
+++ app/Http/Controllers/Admin/WorkController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\WorkRequest;
+use App\Models\Work;
+
+class WorkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\WorkRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(WorkRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Work  $work
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Work $work)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Work  $work
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Work $work)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\WorkRequest  $request
+     * @param  \App\Models\Work  $work
+     * @return \Illuminate\Http\Response
+     */
+    public function update(WorkRequest $request, Work $work)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Work  $work
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Work $work)
+    {
+        //
+    }
+}
