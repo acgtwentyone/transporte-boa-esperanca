9c98410b2680b053f95b20bc531b1685bfeffe95
António Gonçalves <acgtwentyone@gmail.com>
2022-05-31 22:22:40 -0100

    Define relationship between clients and works

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Work;
 
 class Client extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $fillable = ['name', 'address', 'phone'];
+
+    public function works() {
+        return $this->hasMany(Work::class);
+    }
 }
--- app/Models/Work.php
+++ app/Models/Work.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Client;
 
 class Work extends Model
 {
     use HasFactory;
+
+    public function client() {
+        return $this->belongsTo(Client::class);
+    }
 }
