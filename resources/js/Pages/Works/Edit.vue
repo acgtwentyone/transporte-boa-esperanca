4fb69c4f7df856bb9c6f40fb7de2eb801a0bcb9d
António Gonçalves <acgtwentyone@gmail.com>
2022-05-31 00:30:58 -0100

    Create Edit.vue

---
+++ resources/js/Pages/Works/Edit.vue
@@ -0,0 +1,12 @@
+<script setup>
+import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
+import { Head, usePage } from "@inertiajs/inertia-vue3";
+import { computed } from "vue";
+import CreateEdit from '@/Pages/Clients/Partials/CreateEdit.vue';
+
+const client = computed(() => usePage().props.value.client);
+</script>
+
+<template>
+  <CreateEdit :client="client" />
+</template>
