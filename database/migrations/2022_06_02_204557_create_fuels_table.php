b7782b6f3fc65ca48d877190e75596f3a83369c2
António Gonçalves <acgtwentyone@gmail.com>
2022-06-02 19:49:30 -0100

    Add fuel

---
+++ database/migrations/2022_06_02_204557_create_fuels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fuels', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date');
+            $table->decimal('value');
+            $table->string('file');
+            $table->string('driver', 191)->nullable();
+            $table->string('plate', 191);
+            $table->longText('obs')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fuels');
+    }
+};
