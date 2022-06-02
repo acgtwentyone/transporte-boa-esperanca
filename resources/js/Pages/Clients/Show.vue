ea893222307a2b8a11a3afbcc42798ec5a032875
António Gonçalves <acgtwentyone@gmail.com>
2022-06-02 19:31:39 -0100

    Update Show.vue

--- resources/js/Pages/Clients/Show.vue
+++ resources/js/Pages/Clients/Show.vue
@@ -5,7 +5,7 @@ import Dropdown from "@/Components/Dropdown.vue";
 import DropdownLink from "@/Components/DropdownLink.vue";
 import { computed } from "vue";
 
-import ListClientWorks from '@/Pages/Works/Partials/List.vue';
+import ListClientWorks from "@/Pages/Works/Partials/List.vue";
 
 const client = computed(() => usePage().props.value.client);
 </script>
@@ -40,37 +40,40 @@ const client = computed(() => usePage().props.value.client);
         </div>
       </div>
     </div>
-    <div class="flex flex-col rounded-md shadow-md mt-8">
-      <div class="flex justify-between w-full items-center">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight p-2">
-          Trabalhos realizados com o cliente
-        </h2>
-        <Dropdown align="right" width="48">
-          <template #trigger>
-            <span class="inline-flex rounded-md cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-                />
-              </svg>
-            </span>
-          </template>
 
-          <template #content>
-            <DropdownLink :href="route('works.new_work', client)" as="button">
-              Adicionar Trabalho
-            </DropdownLink>
-          </template>
-        </Dropdown>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
+      <div class="flex flex-col rounded-md shadow-md mt-8">
+        <div class="flex justify-between w-full items-center">
+          <h2 class="font-semibold text-xl text-gray-800 leading-tight p-2">
+            Trabalhos realizados com o cliente
+          </h2>
+          <Dropdown class="float-right" width="48">
+            <template #trigger>
+              <span class="inline-flex rounded-md cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                  />
+                </svg>
+              </span>
+            </template>
+
+            <template #content>
+              <DropdownLink :href="route('works.new_work', client)" as="button">
+                Adicionar Trabalho
+              </DropdownLink>
+            </template>
+          </Dropdown>
+        </div>
       </div>
       <ListClientWorks :recent="false" :from_client="1" />
     </div>
