ca3c0e9b826567007a0c9c55b8395a245e69137b
António Gonçalves <acgtwentyone@gmail.com>
2022-06-02 22:01:52 -0100

    Update AdminController.php

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -7,6 +7,8 @@ use Inertia\Inertia;
 use App\Http\Controllers\Controller;
 use App\Models\Client;
 use App\Models\Work;
+use App\Models\Invoice;
+use App\Models\Fuel;
 
 class AdminController extends Controller
 {
@@ -22,6 +24,8 @@ class AdminController extends Controller
     {
         $clients = Client::orderByDesc('created_at'); 
         $works = Work::orderByDesc('created_at'); 
+        $invoices = Invoice::orderByDesc('created_at'); 
+        $fuels = Fuel::orderByDesc('created_at'); 
 
         return Inertia::render('AdminDashboard', [
             'clients' => $clients->take(5)->get()->map(function($client) {
@@ -37,12 +41,20 @@ class AdminController extends Controller
                 return [
                     'id' => $work->id,
                     'date' => $work->date,
+                    'material' => $work->material,
                     'place' => $work->place,
-                    'created_at' => $work->created_at->toDateTimeString(), 
+                    'freight_value' => $work->freight_value,
+                    'paid' => $work->paid,
+                    'debt_value' => $work->debt_value,
+                    'price' => $work->price,
+                    'debt_date' => $work->debt_date,
+                    'created_at' => $work->created_at->toDateTimeString(),
                 ];
             }),
             'total_clients' => $clients->count(),
             'total_works' => $works->count(),
+            'total_invoices' => $invoices->count(),
+            'total_fuels' => $fuels->count(),
         ]);
     }
 }
