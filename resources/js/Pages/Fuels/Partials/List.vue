dbfdb0ea31bc8bde41eb65135f332c469cb535a0
António Gonçalves <acgtwentyone@gmail.com>
2022-06-03 22:54:51 -0100

    Change attach icon and color

--- resources/js/Pages/Fuels/Partials/List.vue
+++ resources/js/Pages/Fuels/Partials/List.vue
@@ -138,7 +138,7 @@ const data = computed(() =>
               <Link :href="route('fuels.edit', fuel)">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6 mx-1 h-6 w-6"
+                  class="h-6 w-6 mx-1 h-6 w-6 text-blue-500"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
