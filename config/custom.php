a115443fcc7c98bbf3434c7e34343994d808e893
António Gonçalves <acgtwentyone@gmail.com>
2022-11-20 12:34:00 +0000

    Update custom.php

--- config/custom.php
+++ config/custom.php
@@ -1,8 +1,14 @@
 <?php
 
 return [
+    // super admin
     "SUPER_ADMIN_EMAIL" => env("SUPER_ADMIN_EMAIL"),
     "SUPER_ADMIN_PASSWORD" => env("SUPER_ADMIN_PASSWORD"),
     "ADMIN_PASSWORD" => env("ADMIN_PASSWORD"),
-    "SUPER_ADMIN_USERNAME" => env("SUPER_ADMIN_USERNAME")
+    "SUPER_ADMIN_USERNAME" => env("SUPER_ADMIN_USERNAME"),
+
+    // nata
+    "NATA_EMAIL" => env("NATA_EMAIL"),
+    "NATA_PASSWORD" => env("NATA_PASSWORD"),
+    "NATA_USERNAME" => env("NATA_USERNAME"),
 ];
